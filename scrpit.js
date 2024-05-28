dc4339e531a8505c63d195d65c110b252bccbc89
sasa9-stack <sasa.igic9@gmail.com>
2024-05-28 10:19:22 +0200

    comments

--- scrpit.js
+++ scrpit.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll('.carousel-slide');
-    const dots = document.querySelectorAll('.dot');
-    const prevButton = document.querySelector('.prev');
+    const slides = document.querySelectorAll('.carousel-slide'); //ovde uzimam slide iz html
+    const dots = document.querySelectorAll('.dot');// ovde navigacijske tacke
+    const prevButton = document.querySelector('.prev');//navigacija
     const nextButton = document.querySelector('.next');
-    let currentIndex = 0;
-    let autoSlideInterval;
+    let currentIndex = 0;// ova varijabla je za broj slajda
+    let autoSlideInterval;// ovo je interval na kojoj se menjaju sljadovi
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
+    function showSlide(index) {//arrow funkcija koja uzima index
+        slides.forEach((slide, i) => {//looping throgh array da vrti slide
             slide.classList.toggle('active', i === index);
         });
 
         dots.forEach((dot, i) => {
-            dot.classList.toggle('active', i === index);
+            dot.classList.toggle('active', i === index);//isto samo dots
         });
     }
 
-    function nextSlide() {
+    function nextSlide() {//ovo je za dugme next slide dodaje +1
         currentIndex = (currentIndex + 1) % slides.length;
         showSlide(currentIndex);
     }
 
-    function prevSlide() {
+    function prevSlide() {//isto samo -1
         currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         showSlide(currentIndex);
     }
 
-    dots.forEach((dot, index) => {
+    dots.forEach((dot, index) => {//ovo ovde nisam siguran za sta je
         dot.addEventListener('click', () => {
             showSlide(index);
             currentIndex = index;
         });
     });
 
-    prevButton.addEventListener('click', prevSlide);
+    prevButton.addEventListener('click', prevSlide);//lagano
     nextButton.addEventListener('click', nextSlide);
 
     // Auto slide
